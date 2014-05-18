e93de319765b905d9c29999cfebd0f118bf302d7
StephenStrickland <sstrick5@spsu.edu>
2014-05-18 19:22:20 -0400

    here is the qmanager

---
+++ WindowsFormsApplicationTVA/WindowsFormsApplicationTVA/QManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace WindowsFormsApplicationTVA
+{
+   
+    class QManager
+    {
+
+ 
+        public string session_connectionString = "mongodb://localhost";
+        public string session_databaseName = "tva_session";
+        public MongoClient session_client;
+        public object clientLock = new object();
+
+        public MongoClient MongoSessionClient
+        {
+            get
+            {
+                lock (clientLock)
+                {
+                    if (session_client != null)
+                        return session_client;
+                    session_client = new MongoClient(session_connectionString);
+                    return session_client;
+
+                }
+            }
+        }
+
+        public MongoCollection<T> GetCollection<T>()
+        {
+            var server = MongoSessionClient.GetServer();
+            var database = server.GetDatabase(session_databaseName);
+            return database.GetCollection<T>(typeof(T).Name);
+
+        }
+
+        }
+
+      
+
+
+        
+       
+    }
+
+    
+
+
